32af662b0ed5841222423deb899e8a9572942ba8
Dev-anish-kr <115490507+Dev-anish-kr@users.noreply.github.com>
2023-05-03 11:54:30 +0530

    Updated

--- src/components/KnowMore.jsx
+++ src/components/KnowMore.jsx
@@ -8,8 +8,8 @@ const KnowMore = () => {
             <header className="public__header">
                 <Link to="/">Home</Link>
             </header>
-            <p className="welc">Welcome to our school! <span className="highlight">We are excited to introduce you to all the amazing things our school has to offer.</span></p>
-            <ul>
+            <p className="welc about">Welcome to our school! <span className="highlight">We are excited to introduce you to all the amazing things our school has to offer.</span></p>
+            <ul className="about">
                 <li>At our school, we are dedicated to providing a high-quality education that prepares our students for success in college, career, and life. We offer a wide range of academic programs, extracurricular activities, and support services to meet the diverse needs and interests of our students.</li>
                 <li>Our faculty and staff are passionate about education and are committed to creating a safe, supportive, and inclusive learning environment for all students. We have a team of experienced and dedicated educators who use a variety of teaching methods and technologies to ensure that each student receives a personalized education that meets their unique needs.</li>
                 <li>Our campus is equipped with modern facilities and technology that enhance the learning experience. We have spacious classrooms, fully-equipped science and computer labs, a library, an auditorium, and a gymnasium. Our campus is also accessible to students with disabilities.</li>
